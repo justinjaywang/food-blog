e63001283abfe8d2951c097178f47632d98bb430
justinjaywang <justinjaywang@gmail.com>
2013-02-26 13:44:20 -0800

    removed build files

--- build/scripts/main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".next").on("click",function(e){if($(this).offsetParent().is(":last-child")){$("html, body").animate({scrollTop:0},500)}else{var t=$(this).offsetParent().next().offset().top;$("html, body").animate({scrollTop:t},500)}});$(".prev").on("click",function(e){var t=$(this).offsetParent().prev().offset().top;$("html, body").animate({scrollTop:t},500)})})
